a9911877caf4d4913907767a1931c5c4b0f5b213
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-06-08 05:29:15 +0500

    lint scripts && husky

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { controllerAuth } from "./controllers/auth";
-import { controllerChatlist } from "./controllers/chatlist";
-import { renderChatList } from "./pages/chatlist/chatlist";
-import { renderErrorPage } from "./pages/errorpage/errorpage";
-import { doLogout, renderLogin } from "./pages/login/login";
-import { renderChangePassword } from "./pages/settings/change-password/change-password";
-import { renderSettings } from "./pages/settings/settings";
-import { renderSettingsEdit } from "./pages/settings/settings-edit/settings-edit";
-import { renderSignup } from "./pages/signup/signup";
-import { router, paths } from "./utils/routes";
+import { controllerAuth } from './controllers/auth.ts'
+import { controllerChatlist } from './controllers/chatlist.ts'
+import { renderChatList } from './pages/chatlist/chatlist.ts'
+import { renderErrorPage } from './pages/errorpage/errorpage.ts'
+import { doLogout, renderLogin } from './pages/login/login.ts'
+import { renderChangePassword } from './pages/settings/change-password/change-password.ts'
+import { renderSettings } from './pages/settings/settings.ts'
+import { renderSettingsEdit } from './pages/settings/settings-edit/settings-edit.ts'
+import { renderSignup } from './pages/signup/signup.ts'
+import { router, paths } from './utils/routes.ts'
 
 window.addEventListener('DOMContentLoaded', async () => {
-
   router
     .use(paths.login, () => renderLogin())
     .use(paths.logout, () => doLogout())
@@ -22,33 +21,20 @@ window.addEventListener('DOMContentLoaded', async () => {
     .use(paths.chatlist, () => renderChatList())
     .use(paths.error404, () => renderErrorPage())
     .use(paths.error500, () => renderErrorPage(500, 'Something broke', 'We are already fixing'))
-
   let isPublicRoute = false
-
-  if (window.location.pathname == paths.login
-    || window.location.pathname == paths.signup) {
+  if (window.location.pathname === paths.login
+    || window.location.pathname === paths.signup) {
     isPublicRoute = true
   }
 
-
-
-
   try {
-    //router.start()  
-
     await controllerAuth.getProfile()
-  
     await controllerChatlist.getChats()
-  
-
-
     router.start()
-
   } catch (e) {
     router.start()
     if (!isPublicRoute) {
       router.go(paths.login)
     }
   }
-
 })
